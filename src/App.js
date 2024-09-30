0395cf6b4153e2deac3d9332fa6eaea8ae42241b
OuaailElaouad <2003waelelaouad@gmail.com>
2024-09-30 22:48:41 +0100

    Posts section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
-import Header from './Header';
+import "./App.css";
+import Header from "./Header";
+import Post from "./Post";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-    
+      <Header />
+
+      <div  style={{}}>
+
+
+      <div style={{ width: "70%" }}>
+        <Post />
+      </div>
+      </div>
     </div>
   );
 }
