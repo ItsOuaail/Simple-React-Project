f7cf8556775637f9b6725a1537a75e9a5c2583a1
OuaailElaouad <2003waelelaouad@gmail.com>
2024-09-30 23:04:43 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import "./App.css";
 import Header from "./Header";
 import Post from "./Post";
+import SideMenu from "./SideMenu";
 
 function App() {
   return (
     <div className="App">
       <Header />
 
-      <div  style={{}}>
+      <div style={{display: "flex", justifyContent: "center"}}>
+        <div style={{ display: "flex", width: "60%", justifyContent: "center" }}>
+          <div style={{ width: "70%" }}>
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+          </div>
 
-
-      <div style={{ width: "70%" }}>
-        <Post />
-      </div>
+          <div style={{ width: "30%", marginTop: "25px" }}>
+            <SideMenu />
+          </div>
+        </div>
       </div>
     </div>
   );
